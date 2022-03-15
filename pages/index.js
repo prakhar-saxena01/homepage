fb6e990556f4bcb074eb1f01124f35ffe2ec2960
prakhar-saxena01 <75985701+prakhar-saxena01@users.noreply.github.com>
2022-03-15 17:57:15 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>The Real Yami</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
@@ -15,13 +11,12 @@ export default function Home() {
         <p className="description">
          This is my hobby website
         </p>
-     
-<a href="https://manga.realyami.in/" target="_blank">manga</a>
-<a href="https://anime.realyami.in/" target="_blank">anime</a>
-<a href="https://stream.realyami.in/" target="_blank">anime-watch</a>
-<a href="https://yt.realyami.in/" target="_blank">youtube</a>
-</main>
-      <Footer />
-    </div>
+    <div class="awesome" style="border: 1px solid red">
+  <label for="name">Enter your name: </label>
+    <iframe
+  src="https://villain.js.org/#/reader?src="
+  style="border: 0"
+  allowFullscreen
+  />
   )
 }
